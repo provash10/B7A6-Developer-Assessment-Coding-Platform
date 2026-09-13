06ced1b994de6ea5bf711594c5af2d362afd6054
Provash <provash20cb@gmail.com>
2026-09-13 12:45:16 +0600

    catchAsymc and jwt

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, RequestHandler, Response } from "express";
 
-const catchAsync = (fn: RequestHandler) => {
+export const catchAsync = (fn: RequestHandler) => {
 	return async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			await fn(req, res, next);
---
+++ src/app/utils/jwt.ts
@@ -0,0 +1,24 @@
+import jwt, { type JwtPayload, type Secret, type SignOptions } from "jsonwebtoken";
+
+export const createToken = (
+	payload: Record<string, unknown>,
+	secret: Secret,
+	expiresIn: SignOptions["expiresIn"],
+): string => {
+	const token = jwt.sign(payload, secret, {
+		expiresIn,
+	} as SignOptions);
+
+	return token;
+};
+
+export const verifyToken = (token: string, secret: Secret): JwtPayload | string => {
+	return jwt.verify(token, secret);
+};
+
+export const jwtUtils = {
+	createToken,
+	verifyToken,
+};
+
+export default jwtUtils;
